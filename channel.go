01429212a16a6822c3e675b3062da4adc4f6487d
Kristóf Jakab <jaksi@jaksi.io>
2016-11-03 16:28:34 +0100

    Parse the payload of channel open requests

--- channel.go
+++ channel.go
@@ -1,14 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/terminal"
 	"log"
 	"net"
 )
 
+// RFC 4254
+type x11 struct {
+	SourceAddress string
+	SourcePort    uint32
+}
+type tcpip struct {
+	DestinationAddress string
+	DestinationPort    uint32
+	SourceAddress      string
+	SourcePort         uint32
+}
+
 func handleNewChannel(remoteAddr net.Addr, newChannel ssh.NewChannel) {
-	log.Printf("New channel: clinet=%v, type=%v, payload=%v\n", remoteAddr, newChannel.ChannelType(), newChannel.ExtraData())
+	var payload string
+	switch newChannel.ChannelType() {
+	case "x11":
+		parsedPayload := x11{}
+		err := ssh.Unmarshal(newChannel.ExtraData(), &parsedPayload)
+		if err != nil {
+			log.Println("Failed to parse payload:", err.Error())
+		}
+		payload = fmt.Sprintf("%+v", parsedPayload)
+	case "forwarded-tcpip":
+		// Server initiated forwarding
+		fallthrough
+	case "direct-tcpip":
+		// Client initiated forwarding
+		parsedPayload := tcpip{}
+		err := ssh.Unmarshal(newChannel.ExtraData(), &parsedPayload)
+		if err != nil {
+			log.Println("Failed to parse payload:", err.Error())
+		}
+		payload = fmt.Sprintf("%+v", parsedPayload)
+	default:
+		payload = fmt.Sprintf("%v", newChannel.ExtraData())
+	}
+	log.Printf("New channel: clinet=%v, type=%v, payload=%v\n", remoteAddr, newChannel.ChannelType(), payload)
 	channel, channelRequests, err := newChannel.Accept()
 	if err != nil {
 		log.Println("Failed to accept channel:", err.Error())
