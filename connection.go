969e79ad70afd8af172150fe14c5e6bc8e8485c9
Kristof Jakab <jaksi@users.noreply.github.com>
2021-05-22 10:49:38 +0100

    Basic global request handling

--- connection.go
+++ connection.go
@@ -9,13 +9,15 @@ import (
 
 func handleConnection(conn net.Conn, sshServerConfig *ssh.ServerConfig) {
 	defer conn.Close()
-	serverConn, newChannels, _, err := ssh.NewServerConn(conn, sshServerConfig)
+	serverConn, newChannels, requests, err := ssh.NewServerConn(conn, sshServerConfig)
 	if err != nil {
 		log.Println("Failed to establish SSH connection:", err)
 		return
 	}
 
 	getLogEntry(serverConn).Infoln("SSH connection established")
+
+	go handleGlobalRequests(requests, serverConn)
 
 	channelID := 0
 	for newChannel := range newChannels {
---
+++ request.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh"
+)
+
+func handleGlobalRequests(requests <-chan *ssh.Request, conn ssh.ConnMetadata) {
+	for request := range requests {
+		if err := request.Reply(true, nil); err != nil {
+			log.Println("Failed to accept global request:", err)
+		}
+
+		getLogEntry(conn).WithFields(logrus.Fields{
+			"request_payload":    request.Payload,
+			"request_type":       request.Type,
+			"request_want_reply": request.WantReply,
+		}).Infoln("Global request accepted")
+	}
+}
