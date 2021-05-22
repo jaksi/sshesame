4a8236c97eee5e31ca16441fdb42d08a94be4b34
Kristof Jakab <jaksi@users.noreply.github.com>
2021-05-22 18:30:06 +0100

    Parse and pretty print global request payloads

--- request.go
+++ request.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 
 	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh"
 )
+
+type tcpipRequestPayload struct {
+	Address string
+	Port    uint32
+}
+
+func (payload tcpipRequestPayload) String() string {
+	return net.JoinHostPort(payload.Address, fmt.Sprint(payload.Port))
+}
 
 func handleGlobalRequests(requests <-chan *ssh.Request, conn ssh.ConnMetadata) {
 	for request := range requests {
@@ -14,8 +25,28 @@ func handleGlobalRequests(requests <-chan *ssh.Request, conn ssh.ConnMetadata) {
 			continue
 		}
 
+		var requestPayload interface{}
+		switch request.Type {
+		case "tcpip-forward":
+			fallthrough
+		case "cancel-tcpip-forward":
+			requestPayload = new(tcpipRequestPayload)
+		default:
+			log.Println("Unsupported global request type", request.Type)
+			continue
+		}
+		requestPayloadString := ""
+		if requestPayload != nil {
+			if err := ssh.Unmarshal(request.Payload, requestPayload); err != nil {
+				log.Println("Failed to parse request payload", err)
+				continue
+			}
+
+			requestPayloadString = fmt.Sprint(requestPayload)
+		}
+
 		getLogEntry(conn).WithFields(logrus.Fields{
-			"request_payload":    request.Payload,
+			"request_payload":    requestPayloadString,
 			"request_type":       request.Type,
 			"request_want_reply": request.WantReply,
 		}).Infoln("Global request accepted")
