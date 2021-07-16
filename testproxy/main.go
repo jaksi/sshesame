4bd407ef899866be144badae9fc8d98a31c1e026
Kristof Jakab <jaksi@users.noreply.github.com>
2021-07-16 14:02:04 +0100

    Proxy: handle and log closed connections

--- testproxy/main.go
+++ testproxy/main.go
@@ -114,6 +114,12 @@ func (entry channelCloseLog) eventType() string {
 	return "channel_close"
 }
 
+type connectionCloseLog struct{}
+
+func (entry connectionCloseLog) eventType() string {
+	return "connection_close"
+}
+
 func logEvent(entry logEntry, src source) {
 	jsonBytes, err := json.Marshal(struct {
 		Source    string   `json:"source"`
@@ -295,19 +301,16 @@ func handleChannel(channelID int, clientChannel ssh.Channel, clientRequests <-ch
 }
 
 func handleConn(clientConn net.Conn, sshServerConfig *ssh.ServerConfig, serverAddress string, clientKey ssh.Signer) {
-	defer clientConn.Close()
-
 	clientSSHConn, clientNewChannels, clientRequests, err := ssh.NewServerConn(clientConn, sshServerConfig)
 	if err != nil {
+		clientConn.Close()
 		panic(err)
 	}
-	defer clientSSHConn.Close()
 
 	serverConn, err := net.Dial("tcp", serverAddress)
 	if err != nil {
 		panic(err)
 	}
-	defer serverConn.Close()
 
 	serverSSHConn, serverNewChannels, serverRequests, err := ssh.NewClientConn(serverConn, serverAddress, &ssh.ClientConfig{
 		User:            clientSSHConn.User(),
@@ -318,9 +321,9 @@ func handleConn(clientConn net.Conn, sshServerConfig *ssh.ServerConfig, serverAd
 		ClientVersion: "SSH-2.0-OpenSSH_7.2",
 	})
 	if err != nil {
+		serverConn.Close()
 		panic(err)
 	}
-	defer serverSSHConn.Close()
 
 	channelID := 0
 
@@ -329,6 +332,12 @@ func handleConn(clientConn net.Conn, sshServerConfig *ssh.ServerConfig, serverAd
 		case clientNewChannel, ok := <-clientNewChannels:
 			if !ok {
 				clientNewChannels = nil
+				if serverNewChannels != nil {
+					logEvent(connectionCloseLog{}, client)
+					if err := serverSSHConn.Close(); err != nil {
+						panic(err)
+					}
+				}
 				continue
 			}
 			serverChannel, serverChannelRequests, err := serverSSHConn.OpenChannel(clientNewChannel.ChannelType(), clientNewChannel.ExtraData())
@@ -386,6 +395,12 @@ func handleConn(clientConn net.Conn, sshServerConfig *ssh.ServerConfig, serverAd
 			}
 		case serverNewChannel, ok := <-serverNewChannels:
 			if !ok {
+				if clientNewChannels != nil {
+					logEvent(connectionCloseLog{}, server)
+					if err := clientSSHConn.Close(); err != nil {
+						panic(err)
+					}
+				}
 				serverNewChannels = nil
 				continue
 			}
